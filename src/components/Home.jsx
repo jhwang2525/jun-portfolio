485edb138108a7f2008b4e8b1d170b02ac8e8635
Jun Hwang <junhwang@Juns-MBP.Starry-inc.net>
2023-03-14 02:15:20 -0700

    about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ const Home = () => {
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Jun Hwang</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front-End Developer </h2>
+        <h1 className='text-3xl sm:text-7xl font-bold text-[#ccd6f6]'>Jun Hwang</h1>
+        <h2 className='text-3xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front-End Developer </h2> 
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a Front-end developer with passion for creating engaging and user-friendly digital experience.
         I'm eager to apply my skills and continue learning and collaboarting with experienced developers and designers
          to develop my skills further and contribute to meaningful projects.</p>
